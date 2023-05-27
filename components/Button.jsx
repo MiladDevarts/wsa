5afd0d78b7c36fc31f0f1ff3f68279e13c412112
Milad Mohamadi <MiladDevarts@gmail.com>
2023-05-27 19:56:44 +0330

    create global button component

---
+++ components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button({ content, classes, outline }) {
+  if (outline) {
+    return <a href="#" className={`flex items-center justify-center w-44 h-16 rounded-full bg-transparent text-white border border-white font-sans ${classes}`}>{content}</a>
+  } else {
+    return <a href="#" className={`flex items-center justify-center w-36 h-16 rounded-full bg-white text-black font-sans ${classes}`}>{content}</a>
+  }
+}
+
+export default Button
