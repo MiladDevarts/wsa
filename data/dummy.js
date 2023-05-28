72a3581ede4a8ca5a386e6fe4fe885ff47a5b2bc
Milad Mohamadi <MiladDevarts@gmail.com>
2023-05-29 03:03:55 +0330

    fix: update dummy json data - add FAQ

--- data/dummy.js
+++ data/dummy.js
@@ -9,5 +9,28 @@ const social = [
     }
 ]
 
+const FAQData = [
+    {
+        title: 'What is White Snail American all about?',
+        content: `Our platform is designed for Iranian people who have any business ideas. Business owners can use our platform to reach their target audience and promote their products or services. Students and educators can use our platform to access educational resources and opportunities. Producers can use this platform to sell their products.`
+    },
+    {
+        title: 'Who can use your platform?',
+        content: `Our platform is designed for Iranian people who have any business ideas. Business owners can use our platform to reach their target audience and promote their products or services. Students and educators can use our platform to access educational resources and opportunities. Producers can use this platform to sell their products.`
+    },
+    {
+        title: 'What types of ideas and startups do you invest in?',
+        content: `Our platform is designed for Iranian people who have any business ideas. Business owners can use our platform to reach their target audience and promote their products or services. Students and educators can use our platform to access educational resources and opportunities. Producers can use this platform to sell their products.`
+    },
+    {
+        title: 'How much funding do you typically provide?',
+        content: `Our platform is designed for Iranian people who have any business ideas. Business owners can use our platform to reach their target audience and promote their products or services. Students and educators can use our platform to access educational resources and opportunities. Producers can use this platform to sell their products.`
+    },
+    {
+        title: 'What other resources do you provide besides funding?',
+        content: `Our platform is designed for Iranian people who have any business ideas. Business owners can use our platform to reach their target audience and promote their products or services. Students and educators can use our platform to access educational resources and opportunities. Producers can use this platform to sell their products.`
+    },
+];
 
-export { social }
+
+export { social, FAQData }
