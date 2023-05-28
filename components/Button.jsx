9e8c75bc8c226a5298862f400d9078d2cda6bab5
Milad Mohamadi <MiladDevarts@gmail.com>
2023-05-28 19:08:28 +0330

    fix: add link prop for button/link comp

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Button({ content, classes, outline }) {
+function Button({ content, classes, outline, link }) {
   if (outline) {
-    return <a href="#" className={`flex items-center justify-center w-44 h-16 rounded-full bg-transparent text-white border border-white font-sans ${classes}`}>{content}</a>
+    return <a href={link} className={`flex items-center justify-center w-44 h-16 rounded-full bg-transparent text-white border border-white font-sans ${classes}`}>{content}</a>
   } else {
-    return <a href="#" className={`flex items-center justify-center w-36 h-16 rounded-full bg-white text-black font-sans ${classes}`}>{content}</a>
+    return <a href={link} className={`flex items-center justify-center w-36 h-16 rounded-full bg-white text-black font-sans ${classes}`}>{content}</a>
   }
 }
 
