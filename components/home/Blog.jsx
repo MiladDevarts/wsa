0cc618ad3b7452bff5d94d71de818eb9e4b7f193
Milad Mohamadi <MiladDevarts@gmail.com>
2023-05-29 17:13:26 +0330

    feat: add blog card component to blog section in home

--- components/home/Blog.jsx
+++ components/home/Blog.jsx
@@ -1,11 +1,46 @@
 
 import TitleSection from "../layout/TitleSection"
+import BlogCard from "./BlogCard"
 
 function Blog() {
   return (
     <>
       <TitleSection title="Blog" link="#" />
-      <section className="container mb-16"></section>
+      <section className="container flex lg:grid lg:grid-cols-2 lg:gap-x-10 lg:gap-y-8 flex-col gap-y-4 mt-10 mb-16">
+
+        <BlogCard
+          image={""}
+          title={"WSA will start getting Clients"}
+          summery={"WSA will start getting ClientsWSA will start getting ClientsWSA will start getting ClientsWSA will start getting ClientsWSA will start getting Clients"}
+          author={"Ryan james"}
+          authorAvatar={"./images/profile/avatar-1.png"}
+          date={"11 December, 2022"} />
+
+          <BlogCard
+          image={"./images/blog/thumbnail-1.png"}
+          title={"WSA will start getting Clients"}
+          summery={"WSA will start getting ClientsWSA will start getting ClientsWSA will start getting ClientsWSA will start getting ClientsWSA will start getting Clients"}
+          author={"Ryan james"}
+          authorAvatar={"./images/profile/avatar-1.png"}
+          date={"11 December, 2022"} />
+
+          <BlogCard
+          image={"./images/blog/thumbnail-1.png"}
+          title={"WSA will start getting Clients"}
+          summery={"WSA will start getting ClientsWSA will start getting ClientsWSA will start getting ClientsWSA will start getting ClientsWSA will start getting Clients"}
+          author={"Ryan james"}
+          authorAvatar={"./images/profile/avatar-1.png"}
+          date={"11 December, 2022"} />
+
+          <BlogCard
+          image={""}
+          title={"WSA will start getting Clients"}
+          summery={"WSA will start getting ClientsWSA will start getting ClientsWSA will start getting ClientsWSA will start getting ClientsWSA will start getting Clients"}
+          author={"Ryan james"}
+          authorAvatar={"./images/profile/avatar-1.png"}
+          date={"11 December, 2022"} />
+
+      </section>
     </>
   )
 }
