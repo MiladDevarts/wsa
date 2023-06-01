01682f498d56a25c4aebb56b5c3b784f20c300ab
Milad Mohamadi <MiladDevarts@gmail.com>
2023-06-01 20:01:58 +0330

    fix: sub-layout fonts url fixed

--- app/about/layout.js
+++ app/about/layout.js
@@ -6,41 +6,53 @@ import NavBar from '@/components/layout/sub/NavBar'
 
 /* --- Normal Typography, Main Body, Paragraph Font Face --- */
 
-const industry = localFont({
+export const yapari = localFont({
   src: [
     {
-      path: '../../public/fonts/IndustryTest-Book.otf',
-      weight: '800'
+      path: '../public/fonts/YapariThin.ttf',
+      weight: '200',
     },
     {
-      path: '../../public/fonts/IndustryTest-Demi.otf',
+      path: '../public/fonts/Yapari-Light.ttf',
+      weight: '300'
+    },
+    {
+      path: '../public/fonts/Yapari-Regular.ttf',
+      weight: '400'
+    },
+    {
+      path: '../public/fonts/Yapari-Bold.ttf',
+      weight: '700'
+    },
+  ],
+  variable: '--font-yapari',
+})
+
+/* --- Normal Typography, Main Body, Paragraph Font Face --- */
+export const industry = localFont({
+  src: [
+    {
+      path: '../public/fonts/IndustryTest-Book.otf',
+      weight: '800',
+    },
+    {
+      path: '../public/fonts/IndustryTest-Demi.otf',
       weight: '700'
     },
     {
-      path: '../../public/fonts/IndustryTest-Medium.otf',
+      path: '../public/fonts/IndustryTest-Medium.otf',
       weight: '500'
     },
   ],
   variable: '--font-industry'
 })
 
-/* --- Big Typography, Main Titles Font Face --- */
-
-const yapari = localFont({
-  src: [
-    {
-      path: '../../public/fonts/Yapari-VF.ttf',
-    },
-  ],
-  variable: '--font-yapari',
-})
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={yapari.className + " " + industry.variable + "relative"}>
-          <NavBar />
-          {children}
+        <NavBar />
+        {children}
       </body>
     </html>
   )
