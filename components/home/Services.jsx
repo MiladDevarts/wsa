5ef06a31f4c8c3a8e73a978c62db4babaea7dea2
Milad Mohamadi <MiladDevarts@gmail.com>
2023-06-16 02:09:32 +0330

    fix: edit animation delay

--- components/home/Services.jsx
+++ components/home/Services.jsx
@@ -9,9 +9,9 @@ function Services() {
             <section className="container mt-4 mb-20 grid lg:grid-cols-3 gap-y-8 lg:gap-x-6">
                {
                 <>
-                    <ServiceCard animationDelay={"200"} showData={true} serviceTitle="Platform Building" servicelink="/services" />
-                    <ServiceCard animationDelay={"300"} showBtn={true} serviceTitle="Brand Development" servicelink="/services" />
-                    <ServiceCard animationDelay={"400"} showBtn={true} serviceTitle="Multipurpose Educational Platform" servicelink="/services"/>
+                    <ServiceCard animationDelay={"100"} showData={true} serviceTitle="Platform Building" servicelink="/services" />
+                    <ServiceCard animationDelay={"150"} showBtn={true} serviceTitle="Brand Development" servicelink="/services" />
+                    <ServiceCard animationDelay={"180"} showBtn={true} serviceTitle="Multipurpose Educational Platform" servicelink="/services"/>
                 </>
                }
             </section>
