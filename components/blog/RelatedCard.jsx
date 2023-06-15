7ecf0adedcd2646ac449ca073cbee61eabb2618c
Milad Mohamadi <MiladDevarts@gmail.com>
2023-06-15 14:47:09 +0330

    fix: edit related post link

--- components/blog/RelatedCard.jsx
+++ components/blog/RelatedCard.jsx
@@ -22,7 +22,7 @@ function RelatedCard({ image, date, author, authorAvatar, title, summery, url })
             <p className="text-[#8D8D8D] my-4">
                 {summery}
             </p>
-            <a href="#" className="flex items-center gap-x-2">
+            <a href="#" className="flex items-center gap-x-2 text-white">
                 Read more
                 <svg width="15" viewBox="0 0 12 11" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M3.44303 8L2.79688 7.35385L7.22764 2.92308H3.25841V2H8.79688V7.53846H7.8738V3.56923L3.44303 8Z" fill="white" />
