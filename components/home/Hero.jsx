05d16c463f4bedf2b28da076d5695c5041c6040d
Milad Mohamadi <MiladDevarts@gmail.com>
2023-06-17 22:01:57 +0330

    fix: edit location box in hero

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -125,15 +125,7 @@ function Hero() {
                         <div class="h-1 bg-white w-1/2"></div>
                     </div>
                 </div>
-                <div className="flex flex-col lg:w-full lg:flex-row lg:justify-end lg:gap-x-5 gap-y-6 items-center mt-32 lg:mt-10">
-                    <div data-aos-delay="70" data-aos="fade-left" className="flex justify-center items-center gap-x-4 text-white">
-                        <svg className="animate-ping " width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="4" cy="4" r="4" fill="#52FF00" />
-                        </svg>
-                        <span className="font-sans">
-                            Currently in Stockholm, New York
-                        </span>
-                    </div>
+                <div className="flex flex-col lg:w-full lg:flex-row lg:justify-end lg:gap-x-5 gap-y-6 items-end mt-32 lg:mt-10">
                     <div className="flex gap-x-3 items-center">
                         <a href="#" data-aos-delay="80" data-aos="fade-left">
                             <svg width="22" height="17" viewBox="0 0 22 17" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -145,6 +137,20 @@ function Hero() {
                                 <path d="M16.19 2H7.81C4.17 2 2 4.17 2 7.81V16.18C2 19.83 4.17 22 7.81 22H16.18C19.82 22 21.99 19.83 21.99 16.19V7.81C22 4.17 19.83 2 16.19 2ZM12 15.88C9.86 15.88 8.12 14.14 8.12 12C8.12 9.86 9.86 8.12 12 8.12C14.14 8.12 15.88 9.86 15.88 12C15.88 14.14 14.14 15.88 12 15.88ZM17.92 6.88C17.87 7 17.8 7.11 17.71 7.21C17.61 7.3 17.5 7.37 17.38 7.42C17.26 7.47 17.13 7.5 17 7.5C16.73 7.5 16.48 7.4 16.29 7.21C16.2 7.11 16.13 7 16.08 6.88C16.03 6.76 16 6.63 16 6.5C16 6.37 16.03 6.24 16.08 6.12C16.13 5.99 16.2 5.89 16.29 5.79C16.52 5.56 16.87 5.45 17.19 5.52C17.26 5.53 17.32 5.55 17.38 5.58C17.44 5.6 17.5 5.63 17.56 5.67C17.61 5.7 17.66 5.75 17.71 5.79C17.8 5.89 17.87 5.99 17.92 6.12C17.97 6.24 18 6.37 18 6.5C18 6.63 17.97 6.76 17.92 6.88Z" fill="white" />
                             </svg>
                         </a>
+                    </div>
+                    <div data-aos-delay="70" data-aos="fade-left" className="flex justify-center items-center gap-x-4 text-white">
+
+                        <div className="font-sans">
+                            <div className="text-[#868686] ml-6">
+                                Currently
+                            </div>
+                            <div className="flex gap-x-4 items-center">
+                                <svg className="animate-ping " width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <circle cx="4" cy="4" r="4" fill="#52FF00" />
+                                </svg>
+                                in Stockholm, New York
+                            </div>
+                        </div>
                     </div>
                     <span className="mt-6 inline lg:hidden">
                         <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
