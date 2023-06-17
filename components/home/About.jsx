0590a093be8b435a2c3dd5f47c18b38ffac90b0e
Milad Mohamadi <MiladDevarts@gmail.com>
2023-06-17 22:28:02 +0330

    fix: edit about section in home page

--- components/home/About.jsx
+++ components/home/About.jsx
@@ -25,14 +25,14 @@ function About() {
     <section className="w-full py-28 lg:py-48 bg-right-top bg-contain bg-no-repeat" style={{ backgroundImage: `url('/images/header/header-bg.png')` }} >
       <div className="container flex flex-col lg:flex-row gap-y-5">
         <div data-aos-delay="110" data-aos="zoom-in-down" className="lg:w-2/5 w-full flex flex-col gap-y-3 lg:-mt-20">
-          <h3 className="text-white text-2xl lg:text-5xl font-bold flex gap-x-2">
+          <h3 className="text-white text-3xl lg:text-5xl font-bold flex gap-x-2 font-mono">
             <svg width="31" height="25" viewBox="0 0 31 25" fill="none" xmlns="http://www.w3.org/2000/svg">
               <rect x="25" width="6" height="24" transform="rotate(24.8486 25 0)" fill="white" />
               <rect x="11" width="6" height="24" transform="rotate(24.8486 11 0)" fill="#5F5F5F" />
             </svg>
             About us
           </h3>
-          <p className="lg:text-4xl text-white font-sans">
+          <p className="text-lg lg:text-4xl text-white font-sans">
             <span className="text-zinc-400 inline-block mr-1">
               Welcome
             </span>
@@ -49,7 +49,7 @@ function About() {
         </div>
         <div data-aos-delay="200" data-aos="zoom-in-up" className="lg:w-3/5 w-full flex flex-col lg:items-end gap-y-4">
           <div className=" flex flex-col gap-y-4">
-            <p className="lg:max-w-2xl lg:text-2xl font-sans text-zinc-400 ">
+            <p className="text-xl lg:max-w-2xl lg:text-2xl font-sans text-zinc-400 ">
               We are
               <span className="text-white inline-block mx-1">a group</span>
                that creates and manages innovative ideas with the help of industry experts. We also support ideas that preserve the biological balance of nature.
