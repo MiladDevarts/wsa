1d9afc38cb7f10ced6c09a27a64920bf91c3d9b2
Milad Mohamadi <MiladDevarts@gmail.com>
2023-06-22 16:03:14 +0330

    fix: edit project title-fs

--- components/home/Projects.jsx
+++ components/home/Projects.jsx
@@ -15,7 +15,7 @@ function Projects() {
     const [sliderRef] = useKeenSlider({
         breakpoints: {
             "(min-width: 160px)": {
-                slides: { perView: 1.2, spacing: 20 },
+                slides: { perView: 1.1, spacing: 20 },
             },
             "(min-width: 768px)": {
                 slides: { perView: 0, spacing: 20 },
@@ -48,9 +48,9 @@ function Projects() {
 
             <section ref={sliderRef} className="keen-slider container mt-4 mb-0 lg:mb-6 flex lg:hidden  text-white">
 
-                <div className="keen-slider__slide text-white w-full h-[550px] flex lg:hidden flex-col justify-center  bg-center bg-no-repeat  bg-contain p-16 gap-y-10" style={{ backgroundImage: `url('/images/projects/project-card-bg.png')` }}>
-                    <span className="font-mono font-bold text-2xl">01</span>
-                    <p className="text-2xl font-bold">
+                <div className="keen-slider__slide text-white w-full h-[550px] flex lg:hidden flex-col justify-center  bg-center bg-no-repeat  bg-contain p-10 gap-y-6" style={{ backgroundImage: `url('/images/projects/project-card-bg.png')` }}>
+                    <span className="font-mono font-bold text-xl">01</span>
+                    <p className="text-xl font-bold">
                         Providing a Multipurpose Educational Platform
                     </p>
                     <p className="leading-8 text-xl font-medium">
@@ -58,19 +58,19 @@ function Projects() {
                     </p>
                 </div>
 
-                <div className="keen-slider__slide w-full h-[550px] flex lg:hidden  flex-col justify-center bg-center bg-no-repeat  bg-contain px-16 py-6 gap-y-6" style={{ backgroundImage: `url('/images/projects/project-card-bg-2.png')` }}>
-                    <span className="font-mono font-bold text-2xl">02</span>
-                    <p className="text-2xl font-bold">
+                <div className="keen-slider__slide w-full h-[550px] flex lg:hidden  flex-col justify-center bg-center bg-no-repeat  bg-contain px-16 p-10 gap-y-6" style={{ backgroundImage: `url('/images/projects/project-card-bg-2.png')` }}>
+                    <span className="font-mono font-bold text-xl">02</span>
+                    <p className="text-xl font-bold">
                         Providing a Multipurpose learning platform
                     </p>
                     <p className="leading-8 text-lg">
-                        At WSA, we contribute to new ideas and production - helping small businesses and professionals...
+                        At WSA, we contribute to new ideas and production...
                     </p>
                 </div>
 
-                <div className="keen-slider__slide w-full h-[550px] flex lg:hidden  flex-col justify-center bg-center bg-no-repeat  bg-contain px-16 py-14 gap-y-6" style={{ backgroundImage: `url('/images/projects/project-card-bg.png')` }}>
-                    <span className="font-mono font-bold text-2xl">03</span>
-                    <p className="text-2xl font-bold">
+                <div className="keen-slider__slide w-full h-[550px] flex lg:hidden  flex-col justify-center bg-center bg-no-repeat  bg-contain px-16 p-10 gap-y-6" style={{ backgroundImage: `url('/images/projects/project-card-bg.png')` }}>
+                    <span className="font-mono font-bold text-xl">03</span>
+                    <p className="text-xl font-bold">
                         Providing a Marketplace Platform
                     </p>
                     <p className="leading-8 text-lg">
